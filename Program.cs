929542a6805cd651e4923908d83a16746c77ed3e
JCHAUDARY\chaud <jose.chaudary@grupomost.com>
2025-06-07 11:43:17 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Delivery.Api.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline (middlewares).
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    WeatherForecast[] forecast = [.. Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))];
+    return Results.Ok(forecast); // or just: return forecast;
+});
+
+await app.RunAsync();
